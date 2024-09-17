debca30bfdbe91455f3cfb491cfa410ab2979247
dlord213 <lollingworld41@gmail.com>
2024-09-17 11:20:19 +0800

    Updated login/register screen

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,13 +1,43 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text, TextInput, View, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+  ToastAndroid,
+} from "react-native";
 import Colors from "@/constants/Colors";
-import { Link, useRouter } from "expo-router";
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { useState } from "react";
 
 export default function Page() {
-  const router = useRouter();
+  const [emailAddress, setEmailAddress] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const validateEmail = (email: string | null) => {
+    if (email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
+    return false;
+  };
+
+  const handleLogin = () => {
+    if (!validateEmail(emailAddress)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    setError(null);
+    ToastAndroid.show(
+      `${emailAddress} - ${password} / Logged In`,
+      ToastAndroid.SHORT
+    );
+  };
 
   return (
     <SafeAreaView style={style.safeAreaView}>
@@ -20,62 +50,49 @@ export default function Page() {
       </Link>
       <View style={{ gap: 8 }}>
         <Text
-          style={[
-            style.boldFont,
-            style.textDefaultColor,
-            {
-              fontSize: 24,
-            },
-          ]}
+          style={[style.boldFont, style.textDefaultColor, { fontSize: 24 }]}
         >
           Login
         </Text>
         <View style={{ flexDirection: "row", gap: 4, alignItems: "center" }}>
           <Text
-            style={[
-              style.regularFont,
-              {
-                color: Colors.Text_Light.Secondary,
-              },
-            ]}
+            style={[style.regularFont, { color: Colors.Text_Light.Secondary }]}
           >
             Don't have an account?
           </Text>
           <Link href="/register" replace>
-            <Text
-              style={[
-                style.semiBoldFont,
-                {
-                  color: Colors.Wewak[600],
-                },
-              ]}
-            >
+            <Text style={[style.semiBoldFont, { color: Colors.Wewak[600] }]}>
               Register
             </Text>
           </Link>
         </View>
       </View>
-      <TextInput placeholder="Email Address" style={style.textInputStyle} />
-      <TextInput placeholder="Password" style={style.textInputStyle} />
+      <TextInput
+        placeholder="Email Address"
+        style={style.textInputStyle}
+        value={emailAddress}
+        onChangeText={setEmailAddress}
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        cursorColor={Colors.Wewak[300]}
+      />
+      {error && <Text style={{ color: "red" }}>{error}</Text>}
+      <TextInput
+        placeholder="Password"
+        style={style.textInputStyle}
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        cursorColor={Colors.Wewak[300]}
+      />
       <Pressable
-        onPress={() => {
-          router.replace("/login");
-        }}
+        onPress={handleLogin}
         style={({ pressed }) => [
           style.pressableStyle,
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
+          { opacity: pressed ? 0.7 : 1 },
         ]}
       >
-        <Text
-          style={[
-            style.regularFont,
-            {
-              color: Colors.Wewak[50],
-            },
-          ]}
-        >
+        <Text style={[style.regularFont, { color: Colors.Wewak[50] }]}>
           Login
         </Text>
       </Pressable>
